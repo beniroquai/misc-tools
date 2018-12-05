7344392fc86083c795ca51577bbd78743e9537b4
beniroquai <bene.d@gmx.de>
2018-12-05 16:09:22 -0500

    Update GenerateGreens_3D.m

--- MATLAB/GenerateGreens_3D.m
+++ MATLAB/GenerateGreens_3D.m
@@ -2,6 +2,7 @@
 % Need DIPIMAGE Toolbox
 % According to https://arxiv.org/abs/1705.04281
 nEmbb = 1.33; 
+nObj = 1.52;
 lambda0 = .6; % measured in µm
 
 Nx = 128;
@@ -9,18 +10,17 @@ Ny = Nx;
 Nz = Nx;
 
 % proper Sampling?!
-deltaX = lambda/8;
-deltaY = deltaX;
-deltaZ = deltaX;
+pixelsize = lambda/4;
+deltaY = pixelsize;
+deltaZ = pixelsize;
 
-offsetZ = 0;
 
 % Compute wave-number in medium
-k0 = 2*pi/lambbda0;
+k0 = 2*pi/lambda0;
 kb = 2*pi/lambda0*sqrt(nEmbb);
 
 mysize = [Nx Ny Nz];
-kr = (rr(mysize)*(1/(Nx*deltaX)));
+kr = (rr(mysize)*(1/(Nx*pixelsize)));
 
 % My approach
 GreensFkt = exp((1i*kb)*abs(kr)) ./ abs(4*pi*kr);
@@ -28,4 +28,11 @@ GreensFkt(MidPosX(GreensFkt),MidPosY(GreensFkt), MidPosZ(GreensFkt))=1.0/sqrt(2)
 GreensFkt_Ft = ft(GreensFkt);
 
 GreensFkt_Ft(:,:,Nz/2)
+
+imshow(abs(double(GreensFkt_Ft(:,:,Nz/2)))), colorbar
+
+% Introduce some sample:
+mysphere = rr(mysize)<8;
+f_obj = nObj*mysphere;
+f = k0^2*(f_obj-nEmbb);
 
